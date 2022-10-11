61980606977c9408e3dd4a1b807ac44d8624609c
philmuhire <philipmuhire@gmail.com>
2022-10-11 12:59:22 +0200

    fixed transactioncontext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <h1 className="text-3xl text-blue-500 font-bold underline">
-        Hello world!
-      </h1>
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
